5ef37fad5a8f4941dc1f5840d074c392e5973e99
pouyapouryaie <pouyapouryaie@gmail.com>
2022-02-06 16:52:50 +0330

    guideline file:
    add some note in code. also write some guide in readme file.

--- order-service/src/main/java/ir/bigz/spring/saga/order/config/OrderPublisherConfig.java
+++ order-service/src/main/java/ir/bigz/spring/saga/order/config/OrderPublisherConfig.java
@@ -3,6 +3,8 @@ package ir.bigz.spring.saga.order.config;
 import ir.bigz.spring.saga.commons.event.OrderEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Sinks;
 
@@ -16,6 +18,7 @@ public class OrderPublisherConfig {
         return Sinks.many().multicast().onBackpressureBuffer();
     }
 
+    /* In order to generate and send events continuously with Spring Cloud Stream Kafka, we need to define a Supplier bean */
     @Bean
     public Supplier<Flux<OrderEvent>> orderSupplier(Sinks.Many<OrderEvent> sinks){
         return sinks::asFlux;
